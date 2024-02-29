d26a200699304fd3017c6d043129b60c19d3855b
WoodMMK <101165135+WoodMMK@users.noreply.github.com>
2024-02-29 22:04:58 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,26 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Node.h"
-
 #include "Queue.h"
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
   NodePtr tailPtr=NULL;
 
-//5 x 9 x 7 x 8 2 x
-/*enqueue(&headPtr,&tailPtr,5);
-printf("%d\n",dequeue(&headPtr,&tailPtr));
-enqueue(&headPtr,&tailPtr,9);
-printf("%d\n",dequeue(&headPtr,&tailPtr));
-enqueue(&headPtr,&tailPtr,7);
-printf("%d\n",dequeue(&headPtr,&tailPtr));
-enqueue(&headPtr,&tailPtr,8);
-enqueue(&headPtr,&tailPtr,2);
-printf("%d\n",dequeue(&headPtr,&tailPtr));*/
-
-
-//For struct Queue
   Queue  q;
    q. headPtr=NULL;
    q.tailPtr=NULL;
